9485881a31f4f83cab8e3bcdbda8be5625219548
nhat <nhatpq@mageplaza.com>
2023-03-30 17:24:22 +0700

    --Create role when creat user company

--- Model/Resolver/User/Create.php
+++ Model/Resolver/User/Create.php
@@ -27,6 +27,7 @@ use Exception;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Mageplaza\CompanyAccounts\Api\Data\UserRolesInterface;
 use Mageplaza\CompanyAccounts\Model\Api\Data\Users;
 use Mageplaza\CompanyAccountsGraphQl\Model\Resolver\AbstractResolver;
 
@@ -43,6 +44,17 @@ class Create extends AbstractResolver
     {
         parent::resolve($field, $context, $info, $value, $args);
 
+        $userRule = $args['input']['mpca_role'][UserRolesInterface::USER_RULES];
+
+        foreach ($userRule as $key => $rule) {
+            $modelRule      = $this->ruleFactory->create()->setData($rule);
+            $userRule[$key] = $modelRule;
+        }
+
+        $this->role                                    = $this->roleFactory->create()->setData($args['input']['mpca_role']);
+
+        //create user
+        unset($args['input']['mpca_role']);
         $user = new Users($args['input']);
 
         try {
@@ -50,6 +62,22 @@ class Create extends AbstractResolver
         } catch (Exception $e) {
             throw new GraphQlInputException(__($e->getMessage()));
         }
+
+        //create user role
+        if (!isset($args['input']['mpca_role_id'])) {
+            try {
+                $resultUserRole = $this->userRolesManagement->createUserRoles($context->getUserId(), $this->role, $args['password']);
+            } catch (Exception $e) {
+                throw new GraphQlInputException(__($e->getMessage()));
+            }
+        }
+
+        $userCompany = \Magento\Framework\App\ObjectManager::getInstance()->create(
+            'Magento\Customer\Api\CustomerRepositoryInterface'
+        );
+        $customer = $userCompany->getById($result->getEntityId());
+        $customer->setCustomAttribute('mpca_role_id',$resultUserRole->getRoleId());
+        $userCompany->save($customer);
 
         return $result;
     }
